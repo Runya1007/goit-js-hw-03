adc86e660eafc823997e85c143a03c9c3956e3dc
Ruslan Shevchenko <runya_@ukr.net>
2024-10-05 17:46:54 +0300

    added task-2

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function makeArray(firstArray, secondArray, maxLength) {
+  const newFirstArray = firstArray.slice(0);
+  const newSecondArray = secondArray.slice(0);
+  const result = newFirstArray.concat(newSecondArray);
+  if (result.length > maxLength) {
+    return result.slice(0, maxLength);
+  }
+  return result;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
